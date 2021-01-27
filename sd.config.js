b41b262318f5547c2e87985519cf29eda97c113d
Jonathan Sick <jsick@lsst.org>
2021-01-27 11:15:02 -0500

    Add custom YAML parser for property files
    This allows us to maintain properties in YAML, which is easier for
    humans to write than JSON in most cases.

---
+++ sd.config.js
@@ -0,0 +1,27 @@
+const yaml = require('yaml');
+
+module.exports = {
+  parsers: [
+    {
+      pattern: /\.yaml$/,
+      // The parse function takes a single argument, which is an object with
+      // 2 attributes: contents which is a string of the file contents, and
+      // filePath which is the path of the file.
+      // The function is expected to return a plain object.
+      parse: ({ contents, filePath }) => yaml.parse(contents),
+    },
+  ],
+  source: [`properties/**/*.yaml`],
+  platforms: {
+    css: {
+      transformGroup: 'css',
+      buildPath: 'build/',
+      files: [
+        {
+          destination: 'variables.css',
+          format: 'css/variables',
+        },
+      ],
+    },
+  },
+};
