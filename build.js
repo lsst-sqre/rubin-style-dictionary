b352df46b3622c934fba9ee5be580af323c43713
Jonathan Sick <jsick@lsst.org>
2021-02-03 20:43:39 -0500

    Add component CTI transform
    Based on https://github.com/amzn/style-dictionary/tree/3.0/examples/advanced/component-cti
    
    This transform applies to all properties under the "component"
    namespace. This transform allows us to namespace properties under the
    "item" rather than the category. Hence this transform effectively swaps
    the "C" and "I" in the style dictionary CTI taxonomy.

--- build.js
+++ build.js
@@ -66,6 +66,47 @@ styleDictionary.registerFormat({
   },
 });
 
+// Map component properties to style-dictionary's CTI taxonomy
+// (In components, the "C" and "I" are reversed).
+// From the component-cti example in style-dictionary
+//
+const propertiesToCTI = {
+  width: { category: 'size', type: 'dimension' },
+  'min-width': { category: 'size', type: 'dimension' },
+  'max-width': { category: 'size', type: 'dimension' },
+  height: { category: 'size', type: 'dimension' },
+  'min-height': { category: 'size', type: 'dimension' },
+  'max-height': { category: 'size', type: 'dimension' },
+  'border-width': { category: 'size', type: 'border', item: 'width' },
+  'border-radius': { category: 'size', type: 'border', item: 'width' },
+  'border-color': { category: 'color', type: 'border' },
+  'background-color': { category: 'color', type: 'background' },
+  color: { category: 'color', type: 'font' },
+  'text-color': { category: 'color', type: 'font' },
+  padding: { category: 'size', type: 'padding' },
+  'padding-vertical': { category: 'size', type: 'padding' },
+  'padding-horizontal': { category: 'size', type: 'padding' },
+  icon: { category: 'content', type: 'icon' },
+  'font-size': { category: 'size', type: 'font' },
+  'line-height': { category: 'size', type: 'line-height' },
+  size: { category: 'size', type: 'icon' },
+};
+
+// Custom transform for components
+const CTITransform = {
+  transformer: (prop) => {
+    // Only do this custom functionality in the 'component' top-level namespace.
+    if (prop.path[0] === 'component') {
+      // When defining component tokens, the key of the token is the relevant CSS property
+      // The key of the token is the last element in the path array
+      return propertiesToCTI[prop.path[prop.path.length - 1]];
+    } else {
+      // Fallback to the original 'attribute/cti' transformer
+      return styleDictionary.transform['attribute/cti'].transformer(prop);
+    }
+  },
+};
+
 const config = {
   parsers: [
     {
@@ -77,6 +118,10 @@ const config = {
       parse: ({ contents, filePath }) => yaml.parse(contents),
     },
   ],
+  transform: {
+    // Override the built-in attribute/cti transform
+    'attribute/cti': CTITransform,
+  },
   source: [`src/**/*.yaml`],
   platforms: {
     css: {
